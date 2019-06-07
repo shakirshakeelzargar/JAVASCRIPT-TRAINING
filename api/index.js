1a19b28eff953c10d70d13be1c41c83671ef008f
Shakir <shakirshakeelzargar11@gmail.com>
2019-06-07 23:48:36 +0530

    SQL CONN SUCCESS

--- api/index.js
+++ api/index.js
@@ -1,49 +1,35 @@
-const express = require('express');
-const app = express();
+
+var express = require('express');
+var app = express();
 var mysql      = require('mysql');
-
-
-
-
 var connection = mysql.createConnection({
-    host     : '148.72.232.93',
-    user     : 'shakir',
-    password : 'shakir123',
-    database : 'shakir_test'
+  host     : 'northside.in',
+  user     : 'shakir',
+  password : 'shakir123',
+  database : 'shakir_test'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT * from emp', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+
+
+app.get('/users', (request, response) => {
+  connection.query('SELECT * FROM emp', (error, result) => {
+      if (error) throw error;
+
+      response.send(result);
   });
-   
-  connection.connect();
-   
-  connection.query('SELECT * FROM EMP', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
-  });
-   
-  connection.end();
-
-
-  app.get('/customers', function (req, res) {
-    connection.connect(sqlConfig, function() {
-        var request = new connection.Request();
-        request.query('select * from EMP', function(err, recordset) {
-            if(err) console.log(err);
-            res.end(JSON.stringify(recordset)); // Result in JSON format
-        });
-    });
-})
-
-
-
-
-/*
-app.get('/',(req, res) => {
-res.send('Hello World');
 });
 
-app.get('/api/courses',(req, res) => {
-    res.send([1,2,3]);
-    });
-*/
+
+
+
+
 app.listen(3000, () => console.log('Listening on port 3000'));
 
 
