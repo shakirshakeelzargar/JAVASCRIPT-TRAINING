30cc8540b5d57db09fbd04a3016788d571db226c
Shakir <shakirshakeelzargar11@gmail.com>
2019-06-07 23:56:02 +0530

    SQL CONN SUCCESS ADDED DELETE PUT

--- api/index.js
+++ api/index.js
@@ -41,6 +41,46 @@ app.get('/users/:id', (request, response) => {
 */
 
 
+/*
+// Add a new user
+app.post('/users', (request, response) => {
+    pool.query('INSERT INTO users SET ?', request.body, (error, result) => {
+        if (error) throw error;
+ 
+        response.status(201).send(`User added with ID: ${result.insertId}`);
+    });
+});
+*/
+
+
+/*
+// Update an existing user
+app.put('/users/:id', (request, response) => {
+    const id = request.params.id;
+ 
+    pool.query('UPDATE users SET ? WHERE id = ?', [request.body, id], (error, result) => {
+        if (error) throw error;
+ 
+        response.send('User updated successfully.');
+    });
+});
+*/
+
+/*
+// Delete a user
+app.delete('/users/:id', (request, response) => {
+    const id = request.params.id;
+ 
+    pool.query('DELETE FROM users WHERE id = ?', id, (error, result) => {
+        if (error) throw error;
+ 
+        response.send('User deleted.');
+    });
+});
+*/
+
+
+
 
 
 
