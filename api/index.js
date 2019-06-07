c31844e967c7a9ab4969f6c4687503a858ac60dd
Shakir Shakeel <shakir.s@softcrylic.co.in>
2019-06-07 13:05:56 +0530

    connecting sql

--- api/index.js
+++ api/index.js
@@ -1,7 +1,41 @@
 const express = require('express');
 const app = express();
+var mysql      = require('mysql');
 
 
+
+
+var connection = mysql.createConnection({
+    host     : '148.72.232.93',
+    user     : 'shakir',
+    password : 'shakir123',
+    database : 'shakir_test'
+  });
+   
+  connection.connect();
+   
+  connection.query('SELECT * FROM EMP', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+  });
+   
+  connection.end();
+
+
+  app.get('/customers', function (req, res) {
+    connection.connect(sqlConfig, function() {
+        var request = new connection.Request();
+        request.query('select * from EMP', function(err, recordset) {
+            if(err) console.log(err);
+            res.end(JSON.stringify(recordset)); // Result in JSON format
+        });
+    });
+})
+
+
+
+
+/*
 app.get('/',(req, res) => {
 res.send('Hello World');
 });
@@ -9,7 +43,7 @@ res.send('Hello World');
 app.get('/api/courses',(req, res) => {
     res.send([1,2,3]);
     });
-
+*/
 app.listen(3000, () => console.log('Listening on port 3000'));
 
 
