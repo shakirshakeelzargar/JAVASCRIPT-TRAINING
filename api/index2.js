c3c64d3e6190206bccfbcf6afc5fed4b11d80244
Shakir <shakirshakeelzargar11@gmail.com>
2019-06-08 16:02:03 +0530

    Index2.js working by using params not worked using body parser

--- api/index2.js
+++ api/index2.js
@@ -1,5 +1,3 @@
-
-    
 var http = require("http");
 var express = require('express');
 var app = express();
@@ -16,7 +14,7 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err
-  console.log('You are now connected with mysql database...')
+  console.log('You are now connected...')
 })
 //end mysql connection
 
@@ -37,43 +35,45 @@ var server = app.listen(3000,  "127.0.0.1", function () {
 
 });
 
-//rest api to get all customers
-app.get('/customer', function (req, res) {
-   connection.query('select * from emp', function (error, results, fields) {
-	  if (error) throw error;
-	  res.end(JSON.stringify(results));
-	});
-});
-//rest api to get a single customer data
-app.get('/customer/:id', function (req, res) {
-   connection.query('select * from emp where Id=?', [req.params.id], function (error, results, fields) {
+//rest api to get all results
+app.get('/employees', function (req, res) {
+   connection.query('select * from employee', function (error, results, fields) {
 	  if (error) throw error;
 	  res.end(JSON.stringify(results));
 	});
 });
 
-//rest api to create a new customer record into mysql database
-app.post('/customer', function (req, res) {
-   var params  = req.body;
-   console.log(params);
-   connection.query('INSERT INTO emp SET ?', params, function (error, results, fields) {
+//rest api to get a single employee data
+app.get('/employees/:id', function (req, res) {
+   console.log(req);
+   connection.query('select * from employee where id=?', [req.params.id], function (error, results, fields) {
+	  if (error) throw error;
+	  res.end(JSON.stringify(results));
+	});
+});
+
+//rest api to create a new record into mysql database
+app.post('/employees/:id/:name/:salary/:age', function (req, res) {
+   //var postData  = req.body;
+   connection.query('INSERT INTO employee SET id=?,name=?,salary=?,age=?',  [req.params.id,req.params.name,req.params.salary, req.params.age], function (error, results, fields) {
 	  if (error) throw error;
 	  res.end(JSON.stringify(results));
 	});
 });
 
 //rest api to update record into mysql database
-app.put('/users/put', function (req, res) {
-   connection.query('UPDATE emp SET Name=?,Address=?', [req.body.Name,req.body.address], function (error, results, fields) {
+app.put('/employees/:id/:name/:salary/:age', function (req, res) {
+   connection.query('UPDATE employee SET name=?,salary=?,age=? where id=?', [req.params.id,req.params.name,req.params.salary, req.params.age], function (error, results, fields) {
 	  if (error) throw error;
 	  res.end(JSON.stringify(results));
 	});
 });
 
 //rest api to delete record from mysql database
-app.delete('/users/delete/:id', function (req, res) {
+app.delete('/employees/:id', function (req, res) {
    console.log(req.body);
-   connection.query('DELETE FROM emp WHERE Id=?', [req.body.Id], function (error, results, fields) {
+   
+   connection.query('DELETE FROM employee WHERE id=?', [req.params.id], function (error, results, fields) {
 	  if (error) throw error;
 	  res.end('Record has been deleted!');
 	});
